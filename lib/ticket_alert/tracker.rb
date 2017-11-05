e41bac5533f52163b98eda03e953e9cbe4727160
Antonio <carrasco.acd@gmail.com>
2017-11-05 11:04:36 +0100

    Remove unused options

--- lib/ticket_alert/tracker.rb
+++ lib/ticket_alert/tracker.rb
@@ -6,7 +6,7 @@ module TicketAlert
  
     def start
       Selenium::WebDriver::Chrome.path = ENV['GOOGLE_CHROME_BIN']
-      @browser = Watir::Browser.new :chrome, options: { args: ['--headless --no-sandbox --disable-gpu --remote-debugging-port=9222'] }
+      @browser = Watir::Browser.new :chrome, options: { args: ['--headless --no-sandbox --disable-gpu'] }
     end
 
     def avaiable_tickets_in? date
